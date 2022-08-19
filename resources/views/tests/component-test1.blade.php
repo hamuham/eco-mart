72be0c3f328a9d0aad576d528ca79f1ba5360fb2
hamuham <uniyui1234@gmail.com>
2022-08-19 19:18:08 +0900

    クラスコンポーネント

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -8,3 +8,7 @@
     <x-tests.card title="CSS変更テスト" class="bg-red-300" />
 
 </x-tests.app>
+
+9.11 午前、夕方
+8.27 16:00
+9.4
--- resources/views/tests/component-test2.blade.php
+++ resources/views/tests/component-test2.blade.php
@@ -3,4 +3,7 @@
         ヘッダー２
     </x-slot>
     コンポーネント２
+    <x-test-class-base classBaseMessage="メッセージ"/>
+    <div class="mb-4 "></div>
+    <x-test-class-base classBaseMessage="メッセージ" defaultMessage="デフォルト変更"/>
 </x-tests.app>

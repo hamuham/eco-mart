7742ad9e178d47ca2497e62e453da257183d18f7
hamuham <uniyui1234@gmail.com>
2022-08-19 18:09:43 +0900

    属性バッグ

--- resources/views/components/tests/card.blade.php
+++ resources/views/components/tests/card.blade.php
@@ -4,7 +4,9 @@
     'content' => '本文初期値'
 ])
 
-<div class="border-2 shadow-md w1/4 p-2">
+<div {{ $asstibutes->merge([
+    'class' => 'border-2 shadow-md w-14 p-2'
+    ]) }} >
     <div>{{ $title }}</div>
     <div>画像</div>
     <div>{{ $content }}</div>
--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -1,7 +1,10 @@
-<x-tests.card>
+<x-tests.app>
     <x-slot name="header">
         ヘッダー１
     </x-slot>
     コンポーネント１
-<x-tests.card title="タイトル" content="本文1" :message="$message">
+    <x-tests.card title="タイトル1" content="本文1" :message="$message" />
+    <x-tests.card title="タイトル2" />
+    <x-tests.card title="CSS変更テスト" class="bg-red-300" />
+
 </x-tests.app>

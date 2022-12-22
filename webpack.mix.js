471ced2082f86336fc22ced4101c93e888c3da06
hamuham <uniyui1234@gmail.com>
2022-12-22 23:45:34 +0900

    webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,5 +12,11 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .react()
-    .sass('resources/sass/app.scss', 'public/css');
+ .react()
+ .js('resources/js/swiper.js', 'public/js')
+ .sass('resources/sass/app.scss', 'public/css')
+ .postCss('resources/css/app.css', 'public/css', [
+ require('postcss-import'),
+ require('tailwindcss'),
+ require('autoprefixer'),
+]);
